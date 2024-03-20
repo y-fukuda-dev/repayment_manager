7df18bb69ecd11194522750f1b06002e8226175b
y-fukuda <jf_7259@outlook.jp>
2024-03-20 20:53:03 +0900

    エラーページを追加

--- src/main/java/com/example/repayment_manager/controller/HomeController.java
+++ src/main/java/com/example/repayment_manager/controller/HomeController.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.repayment_manager.model.RepaymentInformation;
 import com.example.repayment_manager.repository.RepaymentInformationRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -42,5 +43,11 @@ public class HomeController {
     public List<RepaymentInformation> getMethodName(@ModelAttribute RepaymentInformation repaymentInformation) {
         return repaymentInformationRepository.findAll();
     }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
+    
     
 }
