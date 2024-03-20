86dd936a2adba2a27185c0ba7f0499d4533ab146
y-fukuda <jf_7259@outlook.jp>
2024-03-20 20:38:43 +0900

    DB接続設定_起動エラー解消(プラグインのバージョン相性が原因）

--- src/main/java/com/example/repayment_manager/RepaymentManagerApplication.java
+++ src/main/java/com/example/repayment_manager/RepaymentManagerApplication.java
@@ -1,11 +1,17 @@
 package com.example.repayment_manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories("com.example.repayment_manager.repository")
+@ComponentScan
+@EntityScan("com.example.repayment_manager.model")
 // (basePackages = "com.example.repayment_manager.repository")
 public class RepaymentManagerApplication {
 
--- src/main/java/com/example/repayment_manager/controller/HomeController.java
+++ src/main/java/com/example/repayment_manager/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.example.repayment_manager.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.repayment_manager.model.RepaymentInformation;
+import com.example.repayment_manager.repository.RepaymentInformationRepository;
 
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    RepaymentInformationRepository repaymentInformationRepository;
 
     @GetMapping("/")
     public String home() {
@@ -32,7 +39,8 @@ public class HomeController {
     }
 
     @GetMapping("/add")
-    public void getMethodName(@ModelAttribute RepaymentInformation repaymentInformation) {
+    public List<RepaymentInformation> getMethodName(@ModelAttribute RepaymentInformation repaymentInformation) {
+        return repaymentInformationRepository.findAll();
     }
     
 }
