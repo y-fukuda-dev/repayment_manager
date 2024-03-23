c5de7f23f65cd7627b6f9dfda11e74d77ef96378
y-fukuda <jf_7259@outlook.jp>
2024-03-23 10:33:58 +0900

    新規追加サービス_1件目の追加ができるように修正

--- src/main/java/com/example/repayment_manager/service/AddRepaymentInformationService.java
+++ src/main/java/com/example/repayment_manager/service/AddRepaymentInformationService.java
@@ -26,15 +26,30 @@ public class AddRepaymentInformationService {
         this.repaymentInformationRepository = repaymentInformationRepository;
     }
 
-
+    /**
+     * addRepaymentInformationメソッド
+     * RepaymentInformationテーブルに新規レコードを追加する。
+     * @param inDto 入力DTO
+     * @return otDto 出力DTO
+     */
     public AddRepaymentInformationServiceOtDto 
             addRepaymentInformation(AddRepaymentInformationServiceInDto inDto) {
 
-        // 返済情報テーブルから項番の最大値を取得する。
-        Long maxNumber = repaymentInformationRepository.findMaxNumber();
-        // 採番 ← 最大値 + 1
-        Long number = maxNumber + 1L;
+        // 返済情報テーブルにレコードが存在するかチェック
+        boolean flag = false;
+        if (!repaymentInformationRepository.findAll().isEmpty()) {
+            flag = true;
+        }
 
+        // 項番の設定
+        Long number = 1L;
+        // レコードがすでに存在する場合、最大値 + 1
+        // レコードが存在しない場合、1とする
+        if (flag) {
+            number += repaymentInformationRepository.findMaxNumber();
+        }
+
+        // RepaymentInformationエンティティ
         RepaymentInformation information = new RepaymentInformation();
         // 項番
         information.setNumber(number);
